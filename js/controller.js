a8a5c0541f3bc18ba81c4e091039450ec3f756a1
sltiedeman <sltiedeman@gmail.com>
2015-12-01 14:05:53 -0500

    More updates

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,11 @@ var carApp = angular.module('carApp', ['ngRoute']);
 
 //global msrp variable 
 var msrp = 0;
+var make = "";
+var model = "";
 var imagePath = "";
+var counter = 0;
+var selectedCars = [];
 
 carApp.config(function($routeProvider, $locationProvider){
 	$routeProvider.when('/', {
@@ -13,6 +17,10 @@ carApp.config(function($routeProvider, $locationProvider){
 		templateUrl: 'enterdata.html',
 		controller: 'carLoanController'
 	}).
+	when('/compare',{
+		templateUrl: 'compare.html',
+		controller: 'carLoanController'
+	}).
 	otherwise({
 		redirectTo: 'enterdata.html'
 	})
@@ -20,19 +28,48 @@ carApp.config(function($routeProvider, $locationProvider){
 
 carApp.controller('carLoanController', function ($scope, $location){
 
+	//carList is list of cars located in object.js
 	$scope.carList = carList;
-	//An array of all the cars selected
-	selectedCars = [];
 	$scope.msrp = msrp;
+	$scope.make = make;
+	$scope.model = model;
 	$scope.imagePath = imagePath;
+	$scope.selectedCars = selectedCars;
 
-	$scope.calculate = function(){
+
+	$scope.chooseCar = function(){
+		//problem starts here!!!!!!!!!!!!!!!!!!!!!!
+		console.log($scope.selectedItem);
+		console.log(carList);
 		selectedCars.push($scope.selectedItem);
 		console.log(selectedCars);
-		msrp = selectedCars[0].msrp;
-		imagePath = selectedCars[0].imageUrl;
+		msrp = selectedCars[counter].msrp;
+		make = selectedCars[counter].make;
+		model = selectedCars[counter].model;
+		imagePath = selectedCars[counter].imageUrl;
 		$location.path('/enterdata');
-
-
 	}
+
+	$scope.calculate = function(){
+		selectedCars[counter].rate = $scope.rate;
+		selectedCars[counter].amountFinanced = $scope.price - $scope.downpmt;
+		//variables declared for future brevity in math formula
+		var loanAmt = selectedCars[counter].amountFinanced;
+		var rate = $scope.rate;
+		var months = $scope.numPayments;
+		selectedCars[counter].numPayments = $scope.numPayments;
+		selectedCars[counter].downpmt = $scope.downpmt;
+		selectedCars[counter].payment = parseInt((loanAmt*(rate / 12))/(1-Math.pow((1+rate/12),-months)));
+		counter++;;
+		$scope.compare();
+	}
+
+	$scope.compare = function(){
+		$location.path('/compare');
+	}
+
+	$scope.changePath = function(){
+		$location.path("/");
+	}
+
 });
