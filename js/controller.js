4565d00b81066a0bce17358d777f1a98ff7cf33d
sltiedeman <sltiedeman@gmail.com>
2015-12-20 20:38:26 -0500

    Updated intro page styling

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,12 @@
+setTimeout(function(){
+	var windowHeight = window.innerHeight;
+	var contentHeight = $('#intro-page').height();
+	console.log(contentHeight);
+	var paddingAdjust = parseInt((windowHeight - contentHeight)/2)
+	$('#intro-page').css('padding-top', paddingAdjust + 'px');
+},10);
+
+
 
 var carApp = angular.module('carApp', ['ngRoute', 'ngMessages']);
 
@@ -12,7 +21,7 @@ var selectedCars = [];
 var globalIndex;
 
 function viewProject(){
-	document.getElementById("intro-page").style.display="none";
+	document.getElementById("intro-page-wrapper").style.display="none";
 }
 
 
